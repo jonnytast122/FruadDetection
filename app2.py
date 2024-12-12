c4722ff58cc2560e49195165a37771d08ee03cd7
Jonny Tast <124511448+jonnytast122@users.noreply.github.com>
2024-12-12 19:28:48 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,12 +4,11 @@ import pickle
 import numpy as np
 
 # Load the model and label encoder
-path = "C:\\Users\\sengo\\Downloads\\Compressed\\Financial_Fraud\\models\\"
 
-with open(path + "xgb_best_model_2.pkl", 'rb') as model_file:
+with open("xgb_best_model_2.pkl", 'rb') as model_file:
     model = pickle.load(model_file)
 
-with open(path + "label_encoder_2.pkl", 'rb') as encoder_file:
+with open("label_encoder_2.pkl", 'rb') as encoder_file:
     label_encoder = pickle.load(encoder_file)
 
 # Define required columns for batch predictions
